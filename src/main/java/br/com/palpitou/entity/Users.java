f7e9168cec3356b496b7160efaa31a43ecd7f0b4
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-07-02 19:42:48 -0300

    feat: entidade user feita, e enum criado

---
+++ src/main/java/br/com/palpitou/entity/Users.java
@@ -0,0 +1,41 @@
+package br.com.palpitou.entity;
+
+import br.com.palpitou.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter @Setter
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String nome;
+
+      @Column(nullable = false)
+      @Email
+      @NotNull
+      private String Email;
+
+       @NotNull
+       private String Senha;
+
+      @Enumerated(EnumType.STRING)
+      private Role role;
+
+       @Enumerated(EnumType.STRING)
+        private Enum Status;
+}
