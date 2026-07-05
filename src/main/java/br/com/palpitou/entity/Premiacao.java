c2a534b922d9234719febbc5dc28ca586c80d930
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-07-04 21:12:37 -0300

    Feat: Criando Premiçao

---
+++ src/main/java/br/com/palpitou/entity/Premiacao.java
@@ -0,0 +1,32 @@
+package br.com.palpitou.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "premiacoes")
+public class Premiacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private int posicao;
+
+    @NotNull
+    private BigDecimal valor;
+
+    @NotBlank
+    private String descricao;
+}
