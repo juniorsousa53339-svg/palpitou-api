3ab3a7b8dc4c9001888fb8a582a4f3fb507b0fc0
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-07-16 20:29:42 -0300

    Feat: Funcionalidades basicas implementadas em jogo

--- src/main/java/br/com/palpitou/controller/JogoController.java
+++ src/main/java/br/com/palpitou/controller/JogoController.java
@@ -1,4 +1,52 @@
 package br.com.palpitou.controller;
 
+import br.com.palpitou.dto.JogoRequest;
+import br.com.palpitou.dto.JogoResponse;
+import br.com.palpitou.service.JogoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jogos")
+@RequiredArgsConstructor
 public class JogoController {
+
+    private final JogoService jogoService;
+
+    @PostMapping
+    public ResponseEntity<JogoResponse> salvar(
+            @RequestBody @Valid JogoRequest request) {
+
+        JogoResponse resposta =
+                jogoService.salvar(request);
+
+        return ResponseEntity.ok(resposta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<JogoResponse>> listarTodos() {
+
+        var resposta =
+                jogoService.listarTodos();
+
+        return ResponseEntity.ok(resposta);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+
+        jogoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JogoResponse> buscar(@PathVariable Long id) {
+
+        var  resposta = jogoService.buscar(id);
+        return ResponseEntity.ok(resposta);
+    }
 }
--- src/main/java/br/com/palpitou/service/JogoService.java
+++ src/main/java/br/com/palpitou/service/JogoService.java
@@ -1,12 +1,63 @@
 package br.com.palpitou.service;
 
 
+
+
+import br.com.palpitou.dto.JogoRequest;
+import br.com.palpitou.dto.JogoResponse;
+import br.com.palpitou.entity.Jogo;
+import br.com.palpitou.mapper.JogoMapper;
 import br.com.palpitou.repository.JogoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class JogoService {
+
     private final JogoRepository jogoRepository;
+    private final JogoMapper jogoMapper;
+
+    public JogoResponse salvar(JogoRequest request) {
+
+        Jogo jogo = jogoMapper.toEntity(request);
+        Jogo jogoSalvo = jogoRepository.save(jogo);
+        return jogoMapper.toResponse(jogoSalvo);
+    }
+
+    public JogoResponse buscar(Long id) {
+
+        Jogo jogo =
+                jogoRepository.findById(id).
+                        orElseThrow(() ->
+                                new RuntimeException
+                                        ("Jogo não encontrado!"));
+
+        return jogoMapper.toResponse(jogo);
+    }
+
+    public List<JogoResponse> listarTodos() {
+        List<Jogo> jogos = jogoRepository.findAll();
+
+        List<JogoResponse> resposta =
+                jogos.stream()
+                        .map(jogoMapper::toResponse)
+                        .toList();
+
+        return resposta;
+    }
+
+    public void delete(Long id) {
+
+        Jogo jogoBd =
+                jogoRepository.findById(id)
+                        .orElseThrow(()
+                                -> new RuntimeException
+                                ("Campeonato não encontrado")
+                        );
+
+        jogoRepository.delete(jogoBd);
+    }
 }
