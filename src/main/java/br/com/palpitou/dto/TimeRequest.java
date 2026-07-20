60bdc6c9a4f7cf102ff75d3fa78cbdeaa148622f
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-07-20 16:26:30 -0300

    feat: implementando-Dto-Time

---
+++ src/main/java/br/com/palpitou/dto/TimeRequest.java
@@ -0,0 +1,13 @@
+package br.com.palpitou.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class TimeRequest {
+
+    private String nome;
+    private String escudo;
+    private String sigla;
+}
---
+++ src/main/java/br/com/palpitou/dto/TimeResponse.java
@@ -0,0 +1,12 @@
+package br.com.palpitou.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class TimeResponse {
+
+    private String nome;
+    private String escudo;
+    private String sigla;
+}
