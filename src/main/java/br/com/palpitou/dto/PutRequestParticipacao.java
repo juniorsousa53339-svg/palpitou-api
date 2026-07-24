c4ebc5e636d0466d460ba45b09a8beb354d5ae76
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-07-24 16:45:57 -0300

    feat: infra de Participação implementada

---
+++ src/main/java/br/com/palpitou/dto/PutRequestParticipacao.java
@@ -0,0 +1,9 @@
+package br.com.palpitou.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PutRequestParticipacao {
+}
---
+++ src/main/java/br/com/palpitou/dto/PutResponseParticipacao.java
@@ -0,0 +1,8 @@
+package br.com.palpitou.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PutResponseParticipacao {
+}
