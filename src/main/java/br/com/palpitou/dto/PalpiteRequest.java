dacbb27c83b5c8f5f370919701dd102867a4b7d4
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-08-03 19:05:50 -0300

    feat: DTOS de Palpite implementado

--- src/main/java/br/com/palpitou/dto/PalpiteRequest.java
+++ src/main/java/br/com/palpitou/dto/PalpiteRequest.java
@@ -1,4 +1,12 @@
 package br.com.palpitou.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class PalpiteRequest {
+
+    private int golsMandante;
+    private int golsVisitante;
+    private Long jogoId;
 }
--- src/main/java/br/com/palpitou/dto/PalpiteResponse.java
+++ src/main/java/br/com/palpitou/dto/PalpiteResponse.java
@@ -1,4 +1,13 @@
 package br.com.palpitou.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PalpiteResponse {
+
+    private int golsMandante;
+    private int golsVisitante;
+    private int pontosObtidos;
 }
