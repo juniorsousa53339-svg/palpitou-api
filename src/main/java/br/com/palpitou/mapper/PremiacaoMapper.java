f4dbe8f91c0d8956f7176897d80746415be7d1e7
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-08-05 11:53:25 -0300

    refactore: Mapper-Premiacao implementado

--- src/main/java/br/com/palpitou/mapper/PremiacaoMapper.java
+++ src/main/java/br/com/palpitou/mapper/PremiacaoMapper.java
@@ -1,7 +1,35 @@
 package br.com.palpitou.mapper;
 
+import br.com.palpitou.dto.PremiacaoRequest;
+import br.com.palpitou.dto.PremiacaoResponse;
+import br.com.palpitou.entity.Bolao;
+import br.com.palpitou.entity.Premiacao;
+import br.com.palpitou.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PremiacaoMapper {
+
+    public Premiacao toEntity(
+            PremiacaoRequest premiacaoRequest,
+            Bolao bolao ,
+            User user
+    ) {
+
+        Premiacao premiacao = new Premiacao();
+        premiacao.setUser(user);
+        premiacao.setBolao(bolao);
+        return premiacao;
+    }
+
+    public PremiacaoResponse toResponse(Premiacao premiacao) {
+
+        PremiacaoResponse response = new PremiacaoResponse();
+        response.setUserId(premiacao.getUser().getId());
+        response.setBolaoId(premiacao.getBolao().getId());
+        response.setDescricao(premiacao.getDescricao());
+        response.setPosicao(premiacao.getPosicao());
+        response.setValor(premiacao.getValor());
+        return response;
+    }
 }
