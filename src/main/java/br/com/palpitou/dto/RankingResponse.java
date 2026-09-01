781e118158e2c90bee515837468e6b9d49044b33
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-09-01 17:56:30 -0300

    Feat: metodo buscarRankingDaRodada feito

--- src/main/java/br/com/palpitou/dto/RankingResponse.java
+++ src/main/java/br/com/palpitou/dto/RankingResponse.java
@@ -1,4 +1,13 @@
 package br.com.palpitou.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RankingResponse {
+
+    private int posicao;
+    private String nome;
+    private int pontuacao;
 }
