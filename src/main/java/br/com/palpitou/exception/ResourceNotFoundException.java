1b8f1d44e796a94c323d75ca932fcd5cf773848d
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-09-10 20:34:40 -0300

    fix: ajusta excecao de recurso nao encontrado

--- src/main/java/br/com/palpitou/exception/ResourceNotFoundException.java
+++ src/main/java/br/com/palpitou/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package br.com.palpitou.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException  extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
